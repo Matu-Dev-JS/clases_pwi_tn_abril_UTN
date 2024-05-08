3e8200fcc278787b37e4b8fac30ca6653de3b1a0
matu.dev <matu.a.gimenez2002@gmail.com>
2024-05-08 20:21:23 -0300

    clase 12

---
+++ clase-11/js.md
@@ -0,0 +1,6 @@
+### Que es Javascript?
+
+Es un lenguaje de programacion interpretado por el v8engine.
+Se ejecuta en el navegador.
+Es un lenguaje de alto nivel.
+Es un lenguaje CASE SENSITIVE (osea entiende las diferencias entre mayusculas y minusculas)
---
+++ clase-12/script.js
@@ -0,0 +1,51 @@
+/* OPERADORES LOGICOS */
+
+/* 
+!: NOT o negacion 
+Nos da el valor booleano opuesto al dato que estoy negando
+*/
+
+
+/* 
+||: OR o O Logico
+Seleccionar
+Si el primer valor es un valor falsy, va a seleccionar el segundo valor
+Si el primer valor es verdadero, va a seleccionar el primer valor
+
+
+console.log(0 || 'pepe' || 'juan')
+*/
+
+
+/* 
+&: ALT + 38
+&&: AND o Y logico
+
+Evalua si el primer valor es true o un falsy 
+    -Si es false:
+        devuelve el primer valor
+    -Si es true:
+        devuelve el segundo valor
+
+    console.log(0 && 7)
+
+    console.log(true && 7)
+
+    var estado = true
+
+
+    estado && console.log('LLego el mensaje')
+    console.log(1 && 1 && 'pepe')
+*/
+
+
+
+
+/* 
+4 && false || 'pepe'   string  'pepe'
+
+8 == '8' || 1 === '1'   boolean true
+
+(null == NaN) || Boolean(NaN)    false
+false
+*/
